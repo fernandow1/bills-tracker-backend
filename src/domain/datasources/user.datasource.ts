25babe71829c0dd6a182c968e087600f7c8669de
Fernando Wabab <wababfernando@gmail.com>
2025-08-10 14:40:26 -0300

    Add UserDataSource abstract class for user data operations

---
+++ src/domain/datasources/user.datasource.ts
@@ -0,0 +1,9 @@
+import { User } from '@domain/entities/user.entity';
+
+export abstract class UserDataSource {
+  abstract getUsers(): Promise<User[]>;
+  abstract createUser(userData: Partial<User>): Promise<User>;
+  abstract getUserById(id: number): Promise<User | null>;
+  abstract updateUser(id: number, userData: Partial<User>): Promise<User>;
+  abstract deleteUser(id: number): Promise<void>;
+}
