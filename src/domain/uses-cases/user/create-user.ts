d4846c2428f445a7a8c8ec56816b9b6599e4a05a
Fernando Wabab <wababfernando@gmail.com>
2025-08-14 12:53:28 -0300

    Implement password hashing in CreateUser use case

--- src/domain/uses-cases/user/create-user.ts
+++ src/domain/uses-cases/user/create-user.ts
@@ -1,4 +1,5 @@
 import { User } from '@domain/entities/user.entity';
+import { PasswordHasher } from '@domain/ports/password-hasher';
 import { UserRepository } from '@domain/repository/user.repository';
 
 export interface CreateUserUseCase {
@@ -6,11 +7,18 @@ export interface CreateUserUseCase {
 }
 
 export class CreateUser {
-  constructor(private userRepository: UserRepository) {
+  constructor(
+    private userRepository: UserRepository,
+    private passwordHasher: PasswordHasher,
+  ) {
     this.userRepository = userRepository;
+    this.passwordHasher = passwordHasher;
   }
 
   async execute(userData: Partial<User>): Promise<User> {
+    if (userData.password) {
+      userData.password = await this.passwordHasher.hash(userData.password);
+    }
     const user = await this.userRepository.create(userData);
     return user;
   }
