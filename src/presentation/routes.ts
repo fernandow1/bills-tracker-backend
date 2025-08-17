be005de7d32e89cf1f74fc3575674cc0d23df79b
Fernando Wabab <wababfernando@gmail.com>
2025-08-17 17:57:08 -0300

    Add PaymentMethodRouter to AppRoutes for payment method management

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import { ShopRouter } from '@presentation/shop/router';
 import { CurrencyRouter } from '@presentation/currency/router';
 import { UserRouter } from '@presentation/user/router';
+import { PaymentMethodRouter } from '@presentation/payment-method/routes';
 
 export const AppRoutes = {
   routes(): Router {
@@ -12,6 +13,7 @@ export const AppRoutes = {
     router.use('/api/shops', ShopRouter.routes());
     router.use('/api/currencies', CurrencyRouter.routes());
     router.use('/api/users', UserRouter.routes());
+    router.use('/api/payment-methods', PaymentMethodRouter.routes());
 
     return router;
   },
