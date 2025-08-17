03a32c7ab727f5231304ef3a6f725b87d68ca55a
Fernando Wabab <wababfernando@gmail.com>
2025-08-17 18:37:42 -0300

    Fix import path for PaymentMethod entity in PaymentMethodDataSourceImpl

--- src/infrastructure/datasource/payment-method/payment-method.datasource.impl.ts
+++ src/infrastructure/datasource/payment-method/payment-method.datasource.impl.ts
@@ -1,7 +1,7 @@
 import { CreatePaymentMethodDTO } from '@domain/dtos/payment-method/create-payment-method.dto';
-import { PaymentMethod } from '@domain/entities/payment-method.entity';
 import { PaymentMethodRepository } from '@domain/repository/payment-method.repository';
 import { AppDataSource } from '@infrastructure/database/connection';
+import { PaymentMethod } from '@infrastructure/database/entities/payment-method.entity';
 
 export class PaymentMethodDataSourceImpl implements PaymentMethodRepository {
   async createPaymentMethod(data: CreatePaymentMethodDTO): Promise<PaymentMethod> {
