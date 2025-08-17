564cabe950350931d743d89947683b96dc3724c6
Fernando Wabab <wababfernando@gmail.com>
2025-08-17 16:16:04 -0300

    Add PaymentMethodDataSource and PaymentMethodRepository for payment method management

---
+++ src/domain/datasources/payment-method.datasource.ts
@@ -0,0 +1,8 @@
+import { CreatePaymentMethodDTO } from '@domain/dtos/payment-method/create-payment-method.dto';
+import { PaymentMethod } from '@domain/entities/payment-method.entity';
+
+export abstract class PaymentMethodDataSource {
+  abstract createPaymentMethod(data: CreatePaymentMethodDTO): Promise<PaymentMethod>;
+  abstract getPaymentMethodById(id: number): Promise<PaymentMethod | null>;
+  abstract getAllPaymentMethods(): Promise<PaymentMethod[]>;
+}
