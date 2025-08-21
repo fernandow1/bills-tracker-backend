2bd8c7f6756fea348d0c5ab24947b963cc05df69
Fernando Wabab <wababfernando@gmail.com>
2025-08-21 14:15:56 -0300

    Refactor Currency entity to include createdAt, updatedAt, and nullable deletedAt fields

--- src/domain/entities/currency.entity.ts
+++ src/domain/entities/currency.entity.ts
@@ -2,12 +2,26 @@ export class Currency {
   id: number;
   code: string;
   name: string;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt?: Date | null;
   symbol?: string | null;
 
-  constructor(id: number, code: string, name: string, symbol?: string | null) {
+  constructor(
+    id: number,
+    code: string,
+    name: string,
+    createdAt: Date,
+    updatedAt: Date,
+    deletedAt?: Date | null,
+    symbol?: string | null,
+  ) {
     this.id = id;
     this.code = code;
     this.name = name;
     this.symbol = symbol;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+    this.deletedAt = deletedAt;
   }
 }
