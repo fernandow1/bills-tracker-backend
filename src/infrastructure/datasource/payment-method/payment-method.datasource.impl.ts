11ff8678351d1cf40fce340b5eeaaa83be797ffc
Fernando Wabab <wababfernando@gmail.com>
2025-08-21 14:05:06 -0300

    Refactor PaymentMethodDataSourceImpl to implement PaymentMethodDataSource interface

--- src/infrastructure/datasource/payment-method/payment-method.datasource.impl.ts
+++ src/infrastructure/datasource/payment-method/payment-method.datasource.impl.ts
@@ -1,9 +1,9 @@
+import { PaymentMethodDataSource } from '@domain/datasources/payment-method.datasource';
 import { CreatePaymentMethodDTO } from '@domain/dtos/payment-method/create-payment-method.dto';
-import { PaymentMethodRepository } from '@domain/repository/payment-method.repository';
 import { AppDataSource } from '@infrastructure/database/connection';
 import { PaymentMethod } from '@infrastructure/database/entities/payment-method.entity';
 
-export class PaymentMethodDataSourceImpl implements PaymentMethodRepository {
+export class PaymentMethodDataSourceImpl implements PaymentMethodDataSource {
   async createPaymentMethod(data: CreatePaymentMethodDTO): Promise<PaymentMethod> {
     return AppDataSource.getRepository(PaymentMethod).save(data);
   }
