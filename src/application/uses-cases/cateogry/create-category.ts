59c05003a2be07cc23ff11a1d647c359a39c5e01
Fernando Wabab <wababfernando@gmail.com>
2025-08-24 18:06:16 -0300

    Add CreateCategory and GetCategories use cases for category management

---
+++ src/application/uses-cases/cateogry/create-category.ts
@@ -0,0 +1,15 @@
+import { CreateCategoryDTO } from '@domain/dtos/category/create-category.dto';
+import { Category } from '@domain/entities/category.entity';
+import { CategoryRepository } from '@domain/repository/category.repository';
+
+export interface CreateCategoryUseCase {
+  execute(category: CreateCategoryDTO): Promise<Category>;
+}
+
+export class CreateCategory implements CreateCategoryUseCase {
+  constructor(private readonly categoryRepository: CategoryRepository) {}
+
+  async execute(category: CreateCategoryDTO): Promise<Category> {
+    return this.categoryRepository.createCategory(category);
+  }
+}
