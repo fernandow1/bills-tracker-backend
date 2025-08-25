d3c765ae2464505eedd0bebb77d902c3be8b108c
Fernando Wabab <wababfernando@gmail.com>
2025-08-24 22:54:10 -0300

    Add BrandCategory entity to establish relationships between Brand and Category

---
+++ src/infrastructure/database/entities/brand-category.entity.ts
@@ -0,0 +1,60 @@
+import {
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Brand } from './brand.entity';
+import { Category } from './category.entity';
+
+@Entity('brand_category')
+export class BrandCategory {
+  @PrimaryColumn({ name: 'id_brand', type: 'int', nullable: false, unsigned: true })
+  idBrand: number;
+
+  @PrimaryColumn({ name: 'id_category', type: 'int', nullable: false, unsigned: true })
+  idCategory: number;
+
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(0)',
+    nullable: false,
+    precision: 0,
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(0)',
+    onUpdate: 'CURRENT_TIMESTAMP(0)',
+    nullable: false,
+    precision: 0,
+  })
+  updatedAt: Date;
+
+  @DeleteDateColumn({
+    name: 'deleted_at',
+    type: 'timestamp',
+    default: null,
+    nullable: true,
+    precision: 0,
+  })
+  deletedAt?: Date | null;
+
+  @ManyToOne(() => Brand, (brand) => brand.categories, {
+    nullable: false,
+  })
+  @JoinColumn({ name: 'id_brand', referencedColumnName: 'id' })
+  brand: Brand;
+
+  @ManyToOne(() => Category, (category) => category.brandCategories, {
+    nullable: false,
+  })
+  @JoinColumn({ name: 'id_category', referencedColumnName: 'id' })
+  category: Category;
+}
