ee1ab11c06c57832b8468d08ffaad73255ceffcc
Fernando Wabab <wababfernando@gmail.com>
2025-09-11 23:34:11 -0300

    refactor: update BillItemDataSource and BillItemRepository to support transaction management

--- src/domain/repository/bill-item.repository.ts
+++ src/domain/repository/bill-item.repository.ts
@@ -1,10 +1,15 @@
 import { CreateBillItemDTO } from '@application/dtos/bill-item/create-bill-item.dto';
 import { UpdateBillItemDTO } from '@application/dtos/bill-item/update-bill-item.dto';
 import { BillItem } from '@domain/entities/bill-item.entity';
+import { QueryRunner } from 'typeorm';
 
 export abstract class BillItemRepository {
-  abstract create(billItem: CreateBillItemDTO): Promise<BillItem>;
-  abstract update(id: number, billItem: UpdateBillItemDTO): Promise<BillItem>;
-  abstract delete(id: number): Promise<void>;
-  abstract findAll(): Promise<BillItem[]>;
+  abstract create(billItem: CreateBillItemDTO, transaction?: QueryRunner): Promise<BillItem>;
+  abstract update(
+    id: number,
+    billItem: UpdateBillItemDTO,
+    transaction?: QueryRunner,
+  ): Promise<BillItem>;
+  abstract delete(id: number, transaction?: QueryRunner): Promise<void>;
+  abstract findAll(transaction?: QueryRunner): Promise<BillItem[]>;
 }
--- src/infrastructure/datasource/bill-item/bill-item.datasource.impl.ts
+++ src/infrastructure/datasource/bill-item/bill-item.datasource.impl.ts
@@ -3,15 +3,26 @@ import { CreateBillItemDTO } from '@application/dtos/bill-item/create-bill-item.
 import { UpdateBillItemDTO } from '@application/dtos/bill-item/update-bill-item.dto';
 import { AppDataSource } from '@infrastructure/database/connection';
 import { BillItem } from '@infrastructure/database/entities/bill-item.entity';
+import { QueryRunner } from 'typeorm';
 
 export class BillItemDatasourceImpl implements BillItemDataSource {
-  async create(billItem: CreateBillItemDTO): Promise<BillItem> {
-    const billItemToSave = AppDataSource.getRepository(BillItem).create(billItem);
-    return await AppDataSource.getRepository(BillItem).save(billItemToSave);
+  async create(billItem: CreateBillItemDTO, transaction?: QueryRunner): Promise<BillItem> {
+    const repo = transaction
+      ? transaction.manager.getRepository(BillItem)
+      : AppDataSource.getRepository(BillItem);
+    const billItemToSave = repo.create(billItem);
+    return await repo.save(billItemToSave);
   }
 
-  async update(id: number, billItem: UpdateBillItemDTO): Promise<BillItem> {
-    const billItemToUpdate = await AppDataSource.getRepository(BillItem).preload({
+  async update(
+    id: number,
+    billItem: UpdateBillItemDTO,
+    transaction?: QueryRunner,
+  ): Promise<BillItem> {
+    const repo = transaction
+      ? transaction.manager.getRepository(BillItem)
+      : AppDataSource.getRepository(BillItem);
+    const billItemToUpdate = await repo.preload({
       id,
       ...billItem,
     });
@@ -23,11 +34,17 @@ export class BillItemDatasourceImpl implements BillItemDataSource {
     return await AppDataSource.getRepository(BillItem).save(billItemToUpdate);
   }
 
-  async delete(id: number): Promise<void> {
-    await AppDataSource.getRepository(BillItem).softDelete(id);
+  async delete(id: number, transaction?: QueryRunner): Promise<void> {
+    const repo = transaction
+      ? transaction.manager.getRepository(BillItem)
+      : AppDataSource.getRepository(BillItem);
+    await repo.softDelete(id);
   }
 
-  async findAll(): Promise<BillItem[]> {
-    return await AppDataSource.getRepository(BillItem).find();
+  async findAll(transaction?: QueryRunner): Promise<BillItem[]> {
+    const repo = transaction
+      ? transaction.manager.getRepository(BillItem)
+      : AppDataSource.getRepository(BillItem);
+    return await repo.find();
   }
 }
