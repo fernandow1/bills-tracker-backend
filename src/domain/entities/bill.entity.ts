0eec0b968e39e8bad933183890783d412c3a9d7d
Fernando Wabab <wababfernando@gmail.com>
2025-09-14 15:50:30 -0300

    feat: implement Bill entity with TypeORM decorators for database mapping

---
+++ src/domain/entities/bill.entity.ts
@@ -0,0 +1,33 @@
+export class Bill {
+  id: number;
+  idShop: number;
+  idCurrency: number;
+  idPaymentMethod: number;
+  idUser: number;
+  total: number;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date | null;
+
+  constructor(
+    id: number,
+    idShop: number,
+    idCurrency: number,
+    idPaymentMethod: number,
+    idUser: number,
+    total: number,
+    createdAt: Date,
+    updatedAt: Date,
+    deletedAt: Date | null,
+  ) {
+    this.id = id;
+    this.idShop = idShop;
+    this.idCurrency = idCurrency;
+    this.idPaymentMethod = idPaymentMethod;
+    this.idUser = idUser;
+    this.total = total;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+    this.deletedAt = deletedAt;
+  }
+}
---
+++ src/infrastructure/database/entities/bill.entity.ts
@@ -0,0 +1,57 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('bill')
+export class Bill {
+  @PrimaryGeneratedColumn({ name: 'id', type: 'int', unsigned: true })
+  id: number;
+
+  @Column({ name: 'id_shop', type: 'int', unsigned: true })
+  idShop: number;
+
+  @Column({ name: 'id_currency', type: 'int', unsigned: true })
+  idCurrency: number;
+
+  @Column({ name: 'id_payment_method', type: 'int', unsigned: true })
+  idPaymentMethod: number;
+
+  @Column({ name: 'id_user', type: 'int', unsigned: true })
+  idUser: number;
+
+  @Column({ name: 'total', type: 'decimal', precision: 10, scale: 2, unsigned: true })
+  total: number;
+
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(0)',
+    nullable: false,
+    precision: 0,
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(0)',
+    onUpdate: 'CURRENT_TIMESTAMP(0)',
+    nullable: false,
+    precision: 0,
+  })
+  updatedAt: Date;
+
+  @DeleteDateColumn({
+    name: 'deleted_at',
+    type: 'timestamp',
+    default: null,
+    nullable: true,
+    precision: 0,
+  })
+  deletedAt?: Date | null;
+}
