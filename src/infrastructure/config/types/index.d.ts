87c82c88d43fc082684bf2188f75fc26abd27578
Fernando Wabab <wababfernando@gmail.com>
2025-09-21 19:34:33 -0300

    feat: add ValidateToken interface and extend Request type with SafeUser

---
+++ src/infrastructure/config/types/index.d.ts
@@ -0,0 +1,9 @@
+import { SafeUser } from '@application/uses-cases/user/types/auth-user.type';
+
+declare module 'express-serve-static-core' {
+  interface Request {
+    user: SafeUser;
+  }
+}
+
+export {};
