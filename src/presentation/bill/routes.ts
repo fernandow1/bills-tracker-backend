dd3fc4f6ded994d11389a0e78f923c3881a63613
Fernando Wabab <wababfernando@gmail.com>
2025-09-21 19:36:26 -0300

    feat: add JWT validation middleware to bill routes for enhanced security

--- src/presentation/bill/routes.ts
+++ src/presentation/bill/routes.ts
@@ -3,6 +3,7 @@ import { NextFunction, Request, Response, Router } from 'express';
 import { BillDataSourceImpl } from '@infrastructure/datasource/bill/bill.datasource.impl';
 import { BillRepositoryImpl } from '@infrastructure/repositories/bill/bill.repository.impl';
 import { BillController } from '@presentation/bill/controller';
+import { validateJwt } from '@infrastructure/http/middlewares/validate-jwt.middleware';
 
 export const BillRouter = {
   routes(): Router {
@@ -12,16 +13,16 @@ export const BillRouter = {
     const billRepository = new BillRepositoryImpl(billDataSource);
     const billController = new BillController(billRepository);
 
-    router.get('/', (req: Request, res: Response, next: NextFunction) => {
+    router.get('/', [validateJwt], (req: Request, res: Response, next: NextFunction) => {
       billController.getAllBills(req, res, next);
     });
-    router.post('/', (req: Request, res: Response, next: NextFunction) => {
+    router.post('/', [validateJwt], (req: Request, res: Response, next: NextFunction) => {
       billController.createBill(req, res, next);
     });
-    router.put('/:id', (req: Request, res: Response, next: NextFunction) => {
+    router.put('/:id', [validateJwt], (req: Request, res: Response, next: NextFunction) => {
       billController.updateBill(req, res, next);
     });
-    router.delete('/:id', (req: Request, res: Response, next: NextFunction) => {
+    router.delete('/:id', [validateJwt], (req: Request, res: Response, next: NextFunction) => {
       billController.deleteBill(req, res, next);
     });
     return router;
