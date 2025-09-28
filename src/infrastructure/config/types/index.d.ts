ec2c7de95dcf37c515bca528531a31a2bb3c9902
Fernando Wabab <wababfernando@gmail.com>
2025-09-27 21:53:20 -0300

    feat: add optional queryFilter property to Request interface for enhanced query handling

--- src/infrastructure/config/types/index.d.ts
+++ src/infrastructure/config/types/index.d.ts
@@ -1,8 +1,10 @@
 import type { SafeUser } from '@application/uses-cases/user/types/auth-user.type';
+import type { QueryFilterDTO } from '@infrastructure/http/dto/query-filter.dto';
 
 declare module 'express-serve-static-core' {
   interface Request {
     user: SafeUser;
+    queryFilter?: QueryFilterDTO;
   }
 }
 
