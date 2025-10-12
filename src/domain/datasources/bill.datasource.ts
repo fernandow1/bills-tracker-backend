7531abad17a17d4e2df238789a52ef0d9e5d81f1
Fernando Wabab <wababfernando@gmail.com>
2025-10-12 20:22:24 -0300

    feat: add search method to BillDataSource and BillRepository for improved bill querying

--- src/domain/datasources/bill.datasource.ts
+++ src/domain/datasources/bill.datasource.ts
@@ -1,9 +1,12 @@
 import { CreateBillDto } from '@application/dtos/bill/create-bill.dto';
 import { UpdateBillDto } from '@application/dtos/bill/update-bill.dto';
+import { Pagination } from '@application/models/pagination.model';
+import { IQueryFilter } from '@application/models/query-filter.model';
 import { Bill } from '@domain/entities/bill.entity';
 
 export abstract class BillDataSource {
   abstract create(bill: CreateBillDto): Promise<Bill>;
+  abstract search(filter: IQueryFilter): Promise<Pagination<Bill>>;
   abstract findById(id: number): Promise<Bill | null>;
   abstract findAll(): Promise<Bill[]>;
   abstract update(id: number, bill: UpdateBillDto): Promise<UpdateBillDto>;
