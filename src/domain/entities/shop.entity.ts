d86e4d78c81ad8c3a1c82b45c214591422bc926f
Fernando Wabab <wababfernando@gmail.com>
2025-11-09 20:45:24 -0300

    feat: refactor Shop entity to replace location with latitude and longitude properties

--- src/domain/entities/shop.entity.ts
+++ src/domain/entities/shop.entity.ts
@@ -2,12 +2,23 @@ export class Shop {
   id: number;
   name: string;
   description?: string | null;
-  location?: string | null;
+  latitude?: number | null;
+  longitude?: number | null;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt?: Date | null;
 
-  constructor(id: number, name: string, description?: string | null, location?: string | null) {
+  constructor(
+    id: number,
+    name: string,
+    description?: string | null,
+    latitude?: number | null,
+    longitude?: number | null,
+  ) {
     this.id = id;
     this.name = name;
     this.description = description;
-    this.location = location;
+    this.latitude = latitude;
+    this.longitude = longitude;
   }
 }
