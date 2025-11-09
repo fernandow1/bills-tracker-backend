07e1de051c72f66d29129dfc58b00fa90076406d
Fernando Wabab <wababfernando@gmail.com>
2025-11-09 20:45:08 -0300

    feat: update createShop method to accept partial shop data

--- src/domain/datasources/shop.datasource.ts
+++ src/domain/datasources/shop.datasource.ts
@@ -2,7 +2,7 @@ import { Shop } from '@domain/entities/shop.entity';
 
 export abstract class ShopDataSource {
   abstract getAllShops(): Promise<Shop[]>;
-  abstract createShop(shopData: Shop): Promise<Shop>;
+  abstract createShop(shopData: Partial<Shop>): Promise<Shop>;
   abstract updateShop(id: number, shopData: Partial<Shop>): Promise<Shop>;
   abstract deleteShop(id: number): Promise<void>;
 }
