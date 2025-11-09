b743bab55eb979c783134b90289ad0f236f3cace
Fernando Wabab <wababfernando@gmail.com>
2025-11-09 20:45:39 -0300

    feat: implement ShopRepository with CRUD operations

---
+++ src/infrastructure/repositories/shop/shop.repository.impl.ts
@@ -0,0 +1,27 @@
+import { ShopDataSource } from '@domain/datasources/shop.datasource';
+import { Shop } from '@domain/entities/shop.entity';
+import { ShopRepository } from '@domain/repository/shop.repository';
+
+export class ShopRepositoryImpl implements ShopRepository {
+  private shopDataSource: ShopDataSource;
+
+  constructor(shopDataSource: ShopDataSource) {
+    this.shopDataSource = shopDataSource;
+  }
+
+  getAllShops(): Promise<Shop[]> {
+    return this.shopDataSource.getAllShops();
+  }
+
+  createShop(shop: Partial<Shop>): Promise<Shop> {
+    return this.shopDataSource.createShop(shop);
+  }
+
+  updateShop(id: number, shop: Partial<Shop>): Promise<Shop> {
+    return this.shopDataSource.updateShop(id, shop);
+  }
+
+  deleteShop(id: number): Promise<void> {
+    return this.shopDataSource.deleteShop(id);
+  }
+}
