1317f11a13d56508209042659f94ea05fc4f3a99
Fernando Wabab <wababfernando@gmail.com>
2025-11-09 20:45:32 -0300

    feat: update createShop and updateShop methods to accept partial Shop data

--- src/domain/repository/shop.repository.ts
+++ src/domain/repository/shop.repository.ts
@@ -2,7 +2,7 @@ import { Shop } from '@domain/entities/shop.entity';
 
 export abstract class ShopRepository {
   abstract getAllShops(): Promise<Shop[]>;
-  abstract createShop(shop: Shop): Promise<Shop>;
-  abstract updateShop(id: number, shop: Shop): Promise<Shop>;
+  abstract createShop(shop: Partial<Shop>): Promise<Shop>;
+  abstract updateShop(id: number, shop: Partial<Shop>): Promise<Shop>;
   abstract deleteShop(id: number): Promise<void>;
 }
