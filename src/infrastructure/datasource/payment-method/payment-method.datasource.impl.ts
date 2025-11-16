d5a65cce485a654642d6b31b1fc193292141e3af
Fernando Wabab <wababfernando@gmail.com>
2025-11-16 15:00:28 -0300

    feat: refactor PaymentMethodDataSourceImpl to use injected DataSource for CRUD operations

--- src/infrastructure/datasource/payment-method/payment-method.datasource.impl.ts
+++ src/infrastructure/datasource/payment-method/payment-method.datasource.impl.ts
@@ -1,18 +1,29 @@
 import { PaymentMethodDataSource } from '@domain/datasources/payment-method.datasource';
 import { CreatePaymentMethodDTO } from '@application/dtos/payment-method/create-payment-method.dto';
-import { AppDataSource } from '@infrastructure/database/connection';
 import { PaymentMethod } from '@infrastructure/database/entities/payment-method.entity';
+import { DataSource } from 'typeorm';
 
 export class PaymentMethodDataSourceImpl implements PaymentMethodDataSource {
-  async createPaymentMethod(data: CreatePaymentMethodDTO): Promise<PaymentMethod> {
-    return AppDataSource.getRepository(PaymentMethod).save(data);
+  constructor(private readonly dataSource: DataSource) {}
+
+  async updatePaymentMethod(id: number, data: Partial<PaymentMethod>): Promise<PaymentMethod> {
+    const existingPaymentMethod = await this.dataSource
+      .getRepository(PaymentMethod)
+      .findOneOrFail({ where: { id } });
+
+    this.dataSource.getRepository(PaymentMethod).merge(existingPaymentMethod, data);
+    return this.dataSource.getRepository(PaymentMethod).save(existingPaymentMethod);
   }
 
-  async getPaymentMethodById(id: number): Promise<PaymentMethod | null> {
-    return AppDataSource.getRepository(PaymentMethod).findOneBy({ id });
+  async deletePaymentMethod(id: number): Promise<void> {
+    await this.dataSource.getRepository(PaymentMethod).softDelete(id);
+  }
+  async createPaymentMethod(data: CreatePaymentMethodDTO): Promise<PaymentMethod> {
+    const paymentMethod = this.dataSource.getRepository(PaymentMethod).create(data);
+    return this.dataSource.getRepository(PaymentMethod).save(paymentMethod);
   }
 
   async getAllPaymentMethods(): Promise<PaymentMethod[]> {
-    return AppDataSource.getRepository(PaymentMethod).find();
+    return this.dataSource.getRepository(PaymentMethod).find();
   }
 }
