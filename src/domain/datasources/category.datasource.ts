9830302a99c5eed19a8f214c23ceb4659910f0c8
Fernando Wabab <wababfernando@gmail.com>
2025-11-23 12:52:10 -0300

    feat: add updateCategory and deleteCategory methods to CategoryDataSource interface

--- src/domain/datasources/category.datasource.ts
+++ src/domain/datasources/category.datasource.ts
@@ -4,4 +4,6 @@ import { Category } from '@domain/entities/category.entity';
 export abstract class CategoryDataSource {
   abstract createCategory(category: CreateCategoryDTO): Promise<Category>;
   abstract getAllCategories(): Promise<Category[]>;
+  abstract updateCategory(id: number, category: Partial<CreateCategoryDTO>): Promise<Category>;
+  abstract deleteCategory(id: number): Promise<void>;
 }
