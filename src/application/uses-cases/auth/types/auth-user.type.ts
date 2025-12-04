e9d4b9568686bd120ce37808f725d14b8f37e66d
Fernando Wabab <wababfernando@gmail.com>
2025-12-04 02:17:53 -0300

    feat(auth): extend AuthUser type and add RefreshTokenResponse type for token management

--- src/application/uses-cases/auth/types/auth-user.type.ts
+++ src/application/uses-cases/auth/types/auth-user.type.ts
@@ -6,4 +6,12 @@ export type AuthUser = {
   user: SafeUser;
   token: string;
   refreshToken: string;
+  expiresIn: number; // Tiempo de expiración del access token en segundos
+};
+
+// Tipo para la respuesta del refresh token
+export type RefreshTokenResponse = {
+  accessToken: string;
+  refreshToken: string;
+  expiresIn: number;
 };
