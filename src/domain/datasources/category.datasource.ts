8b7eeba60459138ed9f7f28b40ec4b232ff7b636
Fernando Wabab <wababfernando@gmail.com>
2025-12-15 18:48:22 -0300

    feat(datasource): add search method to Brand and Category data sources for improved filtering

--- src/domain/datasources/category.datasource.ts
+++ src/domain/datasources/category.datasource.ts
@@ -1,7 +1,10 @@
 import { CreateCategoryDTO } from '@application/dtos/category/create-category.dto';
+import { Pagination } from '@application/models/pagination.model';
+import { IQueryFilter } from '@application/models/query-filter.model';
 import { Category } from '@domain/entities/category.entity';
 
 export abstract class CategoryDataSource {
+  abstract search(filter: IQueryFilter): Promise<Pagination<Category>>;
   abstract createCategory(category: CreateCategoryDTO): Promise<Category>;
   abstract getAllCategories(): Promise<Category[]>;
   abstract updateCategory(id: number, category: Partial<CreateCategoryDTO>): Promise<Category>;
