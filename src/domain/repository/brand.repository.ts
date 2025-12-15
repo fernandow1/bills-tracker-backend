b9e32ec3ca76158b38d3d18f73954ce9bfe26fa3
Fernando Wabab <wababfernando@gmail.com>
2025-12-15 18:48:07 -0300

    feat(repository): add search method to Brand and Category repositories for improved filtering

--- src/domain/repository/brand.repository.ts
+++ src/domain/repository/brand.repository.ts
@@ -1,7 +1,10 @@
 import { CreateBrandDTO } from '@application/dtos/brand/create-brand.dto';
+import { Pagination } from '@application/models/pagination.model';
+import { IQueryFilter } from '@application/models/query-filter.model';
 import { Brand } from '@domain/entities/brand.entity';
 
 export abstract class BrandRepository {
+  abstract search(filter: IQueryFilter): Promise<Pagination<Brand>>;
   abstract create(brand: CreateBrandDTO): Promise<Brand>;
   abstract update(id: number, dto: Partial<Brand>): Promise<Brand>;
   abstract findAll(): Promise<Brand[]>;
