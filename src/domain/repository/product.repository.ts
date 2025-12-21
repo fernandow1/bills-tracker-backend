8e64e196053fb29f2cd82efa254c98e8876fe645
Fernando Wabab <wababfernando@gmail.com>
2025-12-21 19:28:59 -0300

    feat(repository): implement search method in ProductRepository and ProductRepositoryImpl

--- src/domain/repository/product.repository.ts
+++ src/domain/repository/product.repository.ts
@@ -1,8 +1,11 @@
 import { CreateProductDTO } from '@application/dtos/product/create-product.dto';
 import { UpdateProductDTO } from '@application/dtos/product/update-product.dto';
+import { Pagination } from '@application/models/pagination.model';
+import { IQueryFilter } from '@application/models/query-filter.model';
 import { Product } from '@domain/entities/product.entity';
 
 export abstract class ProductRepository {
+  abstract search(filter: IQueryFilter): Promise<Pagination<Product>>;
   abstract createProduct(product: CreateProductDTO): Promise<Product>;
   abstract getProductById(id: number): Promise<Product>;
   abstract updateProduct(id: number, product: UpdateProductDTO): Promise<UpdateProductDTO>;
--- src/infrastructure/repositories/product/product.repository.impl.ts
+++ src/infrastructure/repositories/product/product.repository.impl.ts
@@ -3,11 +3,17 @@ import { CreateProductDTO } from '@application/dtos/product/create-product.dto';
 import { UpdateProductDTO } from '@application/dtos/product/update-product.dto';
 import { Product } from '@domain/entities/product.entity';
 import { ProductRepository } from '@domain/repository/product.repository';
+import { IQueryFilter } from '@application/models/query-filter.model';
+import { Pagination } from '@application/models/pagination.model';
 
 export class ProductRepositoryImpl implements ProductRepository {
   private readonly productDataSource: ProductDataSource;
   constructor(productDataSource: ProductDataSource) {
     this.productDataSource = productDataSource;
+  }
+
+  search(filter: IQueryFilter): Promise<Pagination<Product>> {
+    return this.productDataSource.search(filter);
   }
 
   createProduct(product: CreateProductDTO): Promise<Product> {
