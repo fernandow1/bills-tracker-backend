08d76211c154649fd8723636e31b2d9ef4d69011
Fernando Wabab <wababfernando@gmail.com>
2025-12-28 16:32:41 -0300

    feat(datasource): add search method to ShopDataSource and ShopRepository for filtering shops

--- src/domain/datasources/shop.datasource.ts
+++ src/domain/datasources/shop.datasource.ts
@@ -1,6 +1,9 @@
+import { Pagination } from '@application/models/pagination.model';
+import { IQueryFilter } from '@application/models/query-filter.model';
 import { Shop } from '@domain/entities/shop.entity';
 
 export abstract class ShopDataSource {
+  abstract serach(filter: IQueryFilter): Promise<Pagination<Shop>>;
   abstract getAllShops(): Promise<Shop[]>;
   abstract createShop(shopData: Partial<Shop>): Promise<Shop>;
   abstract updateShop(id: number, shopData: Partial<Shop>): Promise<Shop>;
