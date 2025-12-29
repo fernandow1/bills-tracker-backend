129e6cb4085ea9f2b178f19f49430117ae5becc9
Fernando Wabab <wababfernando@gmail.com>
2025-12-28 21:35:25 -0300

    feat(shop): add description field to ShopWhere interface and allowed fields

--- src/application/queries/shop/shop-where.ts
+++ src/application/queries/shop/shop-where.ts
@@ -1,6 +1,7 @@
 export interface ShopWhere {
   id?: number;
   name?: string;
+  description?: string;
   createdAt?: Date;
   updatedAt?: Date;
 }
@@ -8,6 +9,7 @@ export interface ShopWhere {
 export const SHOP_ALLOWED_FIELDS = new Map<string, { relation?: string; relationField?: string }>([
   ['id', {}],
   ['name', {}],
+  ['description', {}],
 ]);
 
 export const SHOP_ALLOWED_OPERATIONS = new Set(['eq', 'in', 'like']);
