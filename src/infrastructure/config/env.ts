86281711d5f28df0449d03ae04e4904a2a05ce62
Fernando Wabab <wababfernando@gmail.com>
2026-01-10 14:32:42 -0300

    fix(env): update environment variable keys for database configuration

--- src/infrastructure/config/env.ts
+++ src/infrastructure/config/env.ts
@@ -5,11 +5,11 @@ import { get } from 'env-var';
 export const envs = {
   PORT_TEST: get('PORT_TEST').default('3001').asPortNumber(),
   PORT: get('PORT').required().asPortNumber(),
-  DB_HOST: get('DB_HOST').required().asString(),
-  DB_PORT: get('DB_PORT').default('3306').asPortNumber(),
-  DB_USER: get('DB_USER').required().asString(),
-  DB_PASSWORD: get('DB_PASSWORD').required().asString(),
-  DB_NAME: get('DB_NAME').required().asString(),
-  DB_ROOT_PASSWORD: get('DB_ROOT_PASSWORD').required().asString(),
+  DB_HOST: get('MYSQLHOST').required().asString(),
+  DB_PORT: get('MYSQLPORT').default('3306').asPortNumber(),
+  DB_USER: get('MYSQLUSER').required().asString(),
+  DB_PASSWORD: get('MYSQLPASSWORD').required().asString(),
+  DB_NAME: get('MYSQLDATABASE').required().asString(),
+  DB_ROOT_PASSWORD: get('DB_ROOT_PASSWORD').asString(),
   JWT_SECRET: get('JWT_SECRET').required().asString(),
 };
