ebc735ce30fcf4e2b5a2d9cfcc4d60477d9ff756
Fernando Wabab <wababfernando@gmail.com>
2026-01-14 20:25:23 -0300

    fix(env): ensure NODE_ENV is set for environment configuration

--- src/infrastructure/database/connection.ts
+++ src/infrastructure/database/connection.ts
@@ -4,6 +4,8 @@ import { DataSource } from 'typeorm';
 import path from 'node:path';
 import { envs } from '@infrastructure/config/env';
 
+const isDevelopment = envs.NODE_ENV === 'development';
+
 export const AppDataSource = new DataSource({
   type: 'mysql',
   host: envs.DB_HOST,
@@ -13,7 +15,9 @@ export const AppDataSource = new DataSource({
   database: envs.DB_NAME,
   synchronize: false,
   logging: false,
-  entities: [path.join(process.cwd(), 'dist', 'infrastructure', 'database', 'entities', '*.js')],
+  entities: isDevelopment
+    ? [path.join(process.cwd(), 'src', 'infrastructure', 'database', 'entities', '*.ts')]
+    : [path.join(process.cwd(), 'dist', 'infrastructure', 'database', 'entities', '*.js')],
   migrationsRun: false,
   migrations: [path.join(process.cwd(), 'migrations', '*.js')],
   subscribers: [],
